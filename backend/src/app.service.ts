7647e3d228b28bc3102b2dbc0af9e43f4e14b517
vincent <vincent.durufle@hotmail.fr>
2024-11-26 16:17:33 +0100

    install prisma + user schema + refacto network

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor() {}
+  getHello() {
     return 'Hello World!';
   }
 }
